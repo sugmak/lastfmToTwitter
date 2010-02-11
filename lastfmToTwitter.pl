ead90f4394daacdfad5f8146e087c644ca9c6be1
Makoto SUGIYAMA <sugiyama.makoto@gmail.com>
2010-02-11 13:14:56 +0900

    Use mirror method to check last update time. ;

--- lastfmToTwitter.pl
+++ lastfmToTwitter.pl
@@ -8,35 +8,32 @@ use XML::RSS::Parser;
 use Net::Twitter::Lite;
 
 # set path for your config file
-my $cfg_file = '/home/hoge/.conf/config.ini';
+my $cfg_file = "$ENV{HOME}/.conf/config.ini";
 
-my $cfg = new Config::Simple($cfg_file)->vars();
+if ( !-f $cfg_file ) {
+    die 'No config file.';
+}
+my $cfg = new Config::Simple($cfg_file)->vars() or die Config::Simple->error();
+my $lastfm_feedurl =
+"http://ws.audioscrobbler.com/1.0/user/$cfg->{'lastfm.username'}/recenttracks.rss";
+my $lastfm_mypage = "http://www.lastfm.jp/user/$cfg->{'lastfm.username'}";
+
 if ( !-f $cfg->{'lastfmToTwitter.local_mirror'} ) {
-    mirror( $cfg->{'lastfm.myfeedurl'},
-        $cfg->{'lastfmToTwitter.local_mirror'} );
+    mirror( $lastfm_feedurl, $cfg->{'lastfmToTwitter.local_mirror'} );
 }
 
-my $parser    = XML::RSS::Parser->new;
-my $prev_feed = $parser->parse_file( $cfg->{'lastfmToTwitter.local_mirror'} );
-my $last_modified = $prev_feed->query('//item')->query('pubDate')->text_content;
-mirror( $cfg->{'lastfm.myfeedurl'}, $cfg->{'lastfmToTwitter.local_mirror'} );
-my $latest_feed = $parser->parse_file( $cfg->{'lastfmToTwitter.local_mirror'} );
-my $pubulish_date =
-  $latest_feed->query('//item')->query('pubDate')->text_content;
-my $latest_tune = $latest_feed->query('//item')->query('title')->text_content;
-
-if ( $pubulish_date eq $last_modified ) {
+if (mirror( $lastfm_feedurl, $cfg->{'lastfmToTwitter.local_mirror'} )==RC_NOT_MODIFIED){
     die 'No update since latest publish';
 }
+my $parser    = XML::RSS::Parser->new;
+my $latest_feed = $parser->parse_file( $cfg->{'lastfmToTwitter.local_mirror'} );
+my $latest_tune = $latest_feed->query('//item')->query('title')->text_content;
 
 my $twitter = Net::Twitter::Lite->new(
     username => $cfg->{'twitter.username'},
     password => $cfg->{'twitter.password'}
 );
-
 my $msg =
-    $cfg->{'lastfmToTwitter.perfix'} . ' "'
-  . $latest_tune . '" '
-  . $cfg->{'lastfmToTwitter.suffix'};
+"$cfg->{'lastfmToTwitter.prefix'} \"$latest_tune\" $cfg->{'lastfmToTwitter.suffix'}";
 $twitter->update($msg);
 
